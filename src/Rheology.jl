dc4e1ca2de295bccbfb78612ef7c8bf946adc3c0
jaolive <olive@geologie.ens.fr>
2023-08-25 16:42:55 +0200

    completed documentation on Maxwell visco-elasticity

--- src/Rheology.jl
+++ src/Rheology.jl
@@ -28,34 +28,51 @@ function f_shear(G)
     return G
 end
  
-# """
-#     *(x, y, z...)
+"""
+    f_relax(G)
 
-# Multiplication operator. `x * y * z *...` calls this function with multiple
-# arguments, i.e. `*(x, y, z...)`.
-# """
+Relaxation part of the deviatoric elastic constitutive update: 
+by definition equal to 1 (no relaxation)
+
+"""
 function f_relax(G)
     return 1.
 end
 
 
 """
-    f_shear(G)
+    f_shear(G,η,Δt)
 
-Deviatoric part of the elastic constitutive update: 
-provides the shear modulus G for use in 
+Deviatoric part of the Maxwell visco-elastic constitutive update: 
+used to compute
 
 ```math    
-Δτ = 2G Δε
+Δτ = 2 G (DeN / (1+DeN)) Δε + Δτ_{RELAX}
 ```
-
+with η the shear viscosity, and DeN a numerical Deborah number, equal to 
+```math    
+DeN = η / (G Δt)
+```
 """
 function f_shear(G,η,Δt) # Maxwell visco-elastic
     DeN=η/(G*Δt) # numerical Deborah number
     return 2.0*G*DeN/(1.0+DeN)
 end
 
- 
+"""
+f_relax(G,η,Δt)
+
+Relaxation part of the deviatoric Maxwell visco-elastic constitutive update: 
+for use in the calculation of 
+```math    
+Δτ = (DeN / (1+DeN)) * τ_{OLD} + Δτ_{CONSTITUTIVE}
+```
+with DeN a numerical Deborah number, equal to 
+```math    
+DeN = η / (G Δt)
+```
+and η the shear viscosity.
+"""
 function f_relax(G,η,Δt)
     DeN=η/(G*Δt) # numerical Deborah number
     return DeN/(1.0+DeN)
