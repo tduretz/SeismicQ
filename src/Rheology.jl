17ddfbfe13bb2ea1f716ad1a6b223f63753f3307
tduretz <duretz@uni-frankfurt.de>
2023-08-28 12:51:14 +0200

    Made Kelvin rheology work and clean up

--- src/Rheology.jl
+++ src/Rheology.jl
@@ -1,20 +1,7 @@
-@doc raw"""
-    f_bulk(K) 
+#---------------------------------------------------#
 
-Volumetric part of the elastic constitutive update: 
-provides the bulk modulus K for use in 
-
-```math    
-Δp = -K ∇⋅v Δt
-```
-
-"""
-function f_bulk(K) 
-    return K
-end
-
-"""
-    f_shear(G)
+@doc """
+    θs(G)
 
 Deviatoric part of the elastic constitutive update: 
 provides the shear modulus G for use in 
@@ -24,24 +11,12 @@ provides the shear modulus G for use in
 ```
 
 """
-function f_shear(G)
-    return G
-end
- 
-"""
-    f_relax(G)
-
-Relaxation part of the deviatoric elastic constitutive update: 
-by definition equal to 1 (no relaxation)
-
-"""
-function f_relax(G)
-    return 1.
+function θs(G,Δt)
+    return 2.0*G*Δt
 end
 
-
-"""
-    f_shear(G,η,Δt)
+@doc """
+θs(G,η,Δt)
 
 Deviatoric part of the Maxwell visco-elastic constitutive update: 
 used to compute
@@ -54,13 +29,26 @@ with η the shear viscosity, and DeN a numerical Deborah number, equal to
 DeN = η / (G Δt)
 ```
 """
-function f_shear(G,η,Δt) # Maxwell visco-elastic
-    DeN=η/(G*Δt) # numerical Deborah number
-    return 2.0*G*DeN/(1.0+DeN)
+function θs(G,η,Δt) # Maxwell visco-elastic
+    DeN = η/(G*Δt) # numerical Deborah number
+    return 2.0*G*DeN/(1.0+DeN)*Δt
 end
 
+#---------------------------------------------------#
+
+@doc raw"""
+    χs(G,Δt)
+
+Relaxation part of the deviatoric elastic constitutive update: 
+by definition equal to 1 (no relaxation)
+
 """
-f_relax(G,η,Δt)
+function χs(G,Δt)
+    return 1.
+end
+
+@doc raw"""
+    χs(G,η,Δt)
 
 Relaxation part of the deviatoric Maxwell visco-elastic constitutive update: 
 for use in the calculation of 
@@ -73,29 +61,59 @@ DeN = η / (G Δt)
 ```
 and η the shear viscosity.
 """
-function f_relax(G,η,Δt)
-    DeN=η/(G*Δt) # numerical Deborah number
+function χs(G,η,Δt)
+    DeN = η/(G*Δt) # numerical Deborah number
     return DeN/(1.0+DeN)
 end
 
+#---------------------------------------------------#
+#---------------------------------------------------#
+#---------------------------------------------------#
+
+@doc raw"""
+    θb(K,Δt) 
+
+Volumetric part of the elastic constitutive update: 
+provides the bulk modulus K for use in 
+
+```math    
+Δp = -K ∇⋅v Δt
+```
+
 """
-f_visc(ηb)
+function θb(K,Δt) 
+    return -K*Δt
+end
+
+@doc raw"""
+θb(K,ηb,Δt)
+
+compute effective bulk viscosity for a Kelvin visco-elastic constitutive update 
+
+"""
+function θb(K,ηb,Δt)
+    return -(Δt*K+ηb)
+end
+
+#---------------------------------------------------#
+
+@doc raw"""
+    χb(K,Δt)
+
+compute effective bulk viscosity linear elastic update: 0.
+"""
+function χb(K,Δt) 
+    return 0.
+end
+
+@doc raw"""
+    χb(K,ηb,Δt)
 
 compute effective bulk viscosity for a Kelvin visco-elastic constitutive update: 
 here obviously it is for a linear update 
 """
-function f_visc(ηb)
+function χb(K,ηb,Δt)
     return ηb
 end
 
-"""
-f_bulk(K,ηb,Δt)
-
-compute effective bulk modulus for a Kelvin visco-elastic constitutive update 
-    using the Fatboy number
-
-"""
-function f_bulk(K,ηb,Δt)
-    Fb_bnum = ηb/K/Δt # numerical bulk fatboy number  
-    return K*(1+Fb_bnum)
-end
+#---------------------------------------------------#
